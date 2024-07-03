5076c1a92063ed4f6a99ac04fa605e0bfd922e0c
Rishav Kumar <151399837+Rishav07-05@users.noreply.github.com>
2024-07-03 22:04:34 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountid = 1234
+let accEmail = "rishav34@gmail.com"
+var accPass = "1664"
+accCity = "Haryana"
+let accstate;
+
+
+accEmail = "chotu89@gmail.com"
+accPass = "9876"
+accCity = "Delhi"
+
+console.log(accountid);
+console.table([accountid,accEmail,accPass,accCity,accstate])
