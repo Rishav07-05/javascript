03e10a54e02ddbce8b7eb778dabc7af11327c7f2
Rishav Kumar <151399837+Rishav07-05@users.noreply.github.com>
2024-07-03 22:08:04 +0530

    Add files via upload

---
+++ 08_dateTime.js
@@ -0,0 +1,40 @@
+// let date = new Date()
+// console.log(date)
+// console.log(date.toString())
+// console.log(date.toDateString())
+// console.log(date.toISOString())
+// console.log(date.toJSON())
+// console.log(date.toLocaleDateString())
+// console.log(date.toLocaleString())
+// console.log(date.toUTCString())
+
+// first way 
+// let myDate = new Date(2024 , 6 , 3)
+// console.log(myDate.toDateString());
+
+// second way 
+// let myDate = new Date("2024-07-03")
+// console.log(myDate.toDateString());
+
+// third way 
+// let myDate = new Date("07-03-2024")
+// console.log(myDate.toDateString());
+
+//time
+
+// let timestamp = Date.now()
+
+// console.log(timestamp);
+// console.log(myDate.getTime());
+
+//converting time
+// console.log(Math.floor(Date.now()/1000))
+
+
+//about localDateString functionalities
+
+// let time = new Date()
+// time.toLocaleDateString('default', {
+//     weekday: "long"
+//     }
+// )
