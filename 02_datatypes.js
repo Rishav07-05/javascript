a59f8c9043af9d92e77175c61980f8aa21968d1c
Rishav Kumar <151399837+Rishav07-05@users.noreply.github.com>
2024-07-03 22:06:10 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all js code as newer version
+
+console.log(3+3)
+console.log("Rishav");
+
+
+let name = "Rishav" //strings
+let age = 19 //integers
+let bool = true // booleans
+let undefined ; //Undefined
+let state = null // intentionally given a value equals to zero
+// symbols => uniquely identifying the values
+console.log(typeof age);
