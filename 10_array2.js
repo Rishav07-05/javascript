0ccd2768369d5d34bbe1a716f6072cc55d2c8c5d
Rishav Kumar <151399837+Rishav07-05@users.noreply.github.com>
2024-07-03 22:08:36 +0530

    Add files via upload

---
+++ 10_array2.js
@@ -0,0 +1,11 @@
+const marvel_heroes = ["thor" , "ironman" , "captain america"]
+const dc_heroes = ["superman" , "flash" , "batman"]
+// marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][2]); // this is an array in array 
+
+// const all_Heroes = marvel_heroes.concat(dc_heroes);
+// console.log(all_Heroes);
+
+// const all_new_heroes = [...marvel_heroes, ...dc_heroes] // spread operator acts as concat function
+// console.log(all_new_heroes)
