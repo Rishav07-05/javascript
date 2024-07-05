eb40e50c2034c4924c77a02575b285773d6ad0f3
Rishav⚔️ <151399837+Rishav07-05@users.noreply.github.com>
2024-07-05 16:34:58 +0530

    Add files via upload

---
+++ 19.js_Execution.js
@@ -0,0 +1,12 @@
+// Js execution context 
+
+// 1. Global Execution Context 
+// 2. Function Execution Context 
+// 3. Evol Execution Context  // mainly used in mongoose
+
+
+// Js is executed in three phase 
+
+//1.Global execution
+// 2. Memory creation phase
+// 3. Execution phase 
