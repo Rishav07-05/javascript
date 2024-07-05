65ce3a0fc189eb3c23150f6b1c6f6fa5d1cdc73a
Rishav⚔️ <151399837+Rishav07-05@users.noreply.github.com>
2024-07-05 16:35:12 +0530

    Add files via upload

---
+++ 20_conditionals.js
@@ -0,0 +1,64 @@
+// if statement 
+//=== triple equal is used for strict check when we need to check for the type .
+
+
+// const user = true 
+
+// if (user) {
+//     console.log("User Logged In")
+// }
+// else{
+//     console.log("User not Logged In");
+// }
+
+
+// const temp = 44
+
+// if(temp < 50){
+//     console.log("Temperature is less than 50");
+// }
+// else{
+//     console.log("Temperature is greater than 50")
+// }
+
+
+// short hand notation 
+
+// const balance = 1000
+
+// if(balance > 600) console.log("You are rich");
+
+
+// multiple statement 
+
+// const score = 1000
+
+// if(score < 300){
+//     console.log("You are noob");
+// }
+// else if(score < 600){
+//     console.log("You are an intermidiate");
+// }
+// else if(score < 800){
+//     console.log("You are in a good rank");
+// }
+// else{
+//     console.log("You are a PRO");
+// }
+
+
+
+// multiple conditions 
+
+// const age = 19
+// const height = 5.9
+// const gender = "Male"
+
+// if(age > 18 && height > 5.3 && gender == "Male"){
+//     console.log("You can buy an item from male section!!");
+// }
+// else{
+//     console.log("You are Gay");
+// }
+
+
