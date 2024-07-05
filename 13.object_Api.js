d3f9e741d1539dfd52c9fa1059a01f61cb26b604
Rishav⚔️ <151399837+Rishav07-05@users.noreply.github.com>
2024-07-05 16:33:04 +0530

    Add files via upload

---
+++ 13.object_Api.js
@@ -0,0 +1,26 @@
+
+
+const course = {
+    coursename: "python for begineer",
+    price: "1000",
+    courseInstructor: "Human"
+}
+// console.log(course.price);
+
+const {price} = course  // Destructuring 
+console.log(price);
+
+
+
+// JSON ==> in JSON the key and value both are written in string format
+// {
+//     "name" = "Rishav",
+//     "course" = "Javascript",
+//     "price" = "free"
+// }
+
+// {
+//     {},
+//     {},
+//     {}
+// }
