e30d3528f54067f1a2a6586978e621d932ae23d4
Rishav Kumar <rishavkumar18902@gmail.com>
2024-12-26 12:27:35 +0530

    file added

---
+++ script.js
@@ -0,0 +1,62 @@
+// alert('Hey')
+
+// forEach
+
+// const names = ["Alice", "Bob", "Charlie", "David", "Emma"];
+// names.forEach((value) => {
+//     if (value === 'Charlie') { }
+//     else {
+//         console.log(value);
+
+//     }
+// })
+
+// Map
+
+// const names = ["Alice", "Bob", "Charlie", "David", "Emma"];
+
+// let newArr = names.map((value) => {
+//     return value + ' ji';
+// })
+
+// console.log(newArr);
+
+
+// filter
+
+// const names = ["Alice", "Bob", "Charlie", "David", "Emma" , 'Akash'];
+
+// let newArr = names.filter((value) => {
+//     return (value.startsWith('A'));
+// })
+
+
+// spread ...
+
+// const names = ['John', 'Jane', 'Doe', 'Smith'];
+
+// const copyNames = [...names];
+
+
+// const smallObj = {
+//     names: "john",
+//     age: 30,
+//     city: "New York",
+// };
+ 
+// const newObj = {...smallObj}
+
+
+// rest... 
+
+// const abcd = (a , b , c , ...rest) => {
+//     console.log(a , b , c , rest);
+    
+// }
+
+
+// abcd(1, 2, 3, 4, 5, 6);
+
+
+
+
